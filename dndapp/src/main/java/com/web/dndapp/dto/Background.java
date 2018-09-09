b6a4b0abda3e67ad5cf861fb9393e5278442b449
Chris <csmith20@dons.usfca.edu>
2018-09-09 15:23:20 -0700

    Set up for data acquisition of classes and backgrounds

---
+++ dndapp/src/main/java/com/web/dndapp/dto/Background.java
@@ -0,0 +1,12 @@
+package com.web.dndapp.dto;
+
+import java.util.List;
+
+public class Background {
+
+	String name;
+	List<String> profiency; // list of skills
+	List<String> tools;
+	List<String> languages;
+	List<String> equipment;
+}
